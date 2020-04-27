7554f5c891c67fd186c86a77994d32d19c6680c4
prabin <prb977@gmail.com>
2020-04-27 20:40:02 +0545

    Small Change

--- image_fields/fields.py
+++ image_fields/fields.py
@@ -1,10 +1,9 @@
 import base64
 import uuid
-from functools import partial
 
 from django.core.files.base import ContentFile
 from rest_framework import serializers
-from rest_framework.serializers import ValidationError
+# from rest_framework.serializers import ValidationError
 
 
 class Base64ContentField(serializers.Field):
@@ -13,17 +12,16 @@ class Base64ContentField(serializers.Field):
     '''
 
     def to_internal_value(self, data):
-        try:
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            # You can save this as file instance.
-            data = ContentFile(
-                base64.b64decode(imgstr),
-                name=str(uuid.uuid4())+'.'+ext
-            )
-            # print(data)
-        except:
-            raise ValidationError('Error in data')
+        # try:
+        format, imgstr = data.split(';base64,')
+        ext = format.split('/')[-1]
+        # You can save this as file instance.
+        data = ContentFile(
+            base64.b64decode(imgstr),
+            name=str(uuid.uuid4())+'.'+ext
+        )
+        # except:
+        #     raise ValidationError('Error in data')
         return data
 
     def to_representation(self, value):
